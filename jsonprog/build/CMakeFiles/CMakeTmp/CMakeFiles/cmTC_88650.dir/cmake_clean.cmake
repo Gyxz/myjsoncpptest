38815d7e908f86b2ba323580c5d3bad8d6caa26d
Maksym Ivasiv <maks1404@gmail.com>
2021-03-11 14:47:26 +0200

    add new snap

---
+++ jsonprog/build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_88650.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_88650.dir/CMakeCXXCompilerABI.cpp.o"
+  "cmTC_88650"
+  "cmTC_88650.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_88650.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ jsonprog/build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_c984b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_c984b.dir/testCXXCompiler.cxx.o"
+  "cmTC_c984b"
+  "cmTC_c984b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_c984b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
